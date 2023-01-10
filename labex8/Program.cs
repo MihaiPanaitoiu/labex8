e4bf86ca4d228244ea8e71a2c8754ff87195dc88
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2023-01-10 23:35:35 +0200

    Add project files.

---
+++ labex8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace labex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Supliment 1
+• Se citesc doua numere de la tastatura. Scrieti un program care va calcula
+cel mai mare divizor comun al numerelor
+             */
+            Console.WriteLine("N=");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("M=");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetCmmmdc(n, m));
+        }
+
+        static int GetCmmmdc(int n, int m)
+        {
+            if (n < m)
+            {
+               return GetCmmmdc(m - n, n);
+            } else if(m < n)
+            {
+                return GetCmmmdc(n - m, m);
+            } else
+            {
+                return n;
+            }
+        }
+
+    }
+}
